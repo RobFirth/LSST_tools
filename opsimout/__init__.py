ab241b5b7281ceb4c8b54efa9a9d4e4c751efaee
Rob Firth <ref1g12@soton.ac.uk>
2017-03-28 18:12:30 +0100

    tweaks

---
+++ opsimout/__init__.py
@@ -0,0 +1,18 @@
+"""
+Tools for looking at OpSim run outputs
+
+Author: Rob Firth, University of Southampton, 03/2017
+"""
+
+from __future__ import print_function ## Force python3-like printing
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+import os
+import time
+
+from sqlalchemy import create_engine
+
+opsimdbpath = os.environ.get('OPSIMDBPATH')
