b4bcd178c5e49809a87738db7d1158ae65d22618
Rob Firth <ref1g12@soton.ac.uk>
2017-04-20 16:41:50 +0100

    added simple plotting routines

--- utils/__init__.py
+++ utils/__init__.py
@@ -81,3 +81,47 @@ def find_MW_extinction(df):
     m = sfdmap.SFDMap()
     df["EBV_MW"] = m.ebv(df["theta"], df["phi"], unit = "radian")
     return df
+
+def plot_position_points(df):
+    """
+    for small realisations! If large, use plot_position_heatmap
+    """
+    fig = plt.figure()
+    fig.subplots_adjust(left = 0.09, bottom = 0.13, top = 0.99,
+                        right = 0.97, hspace=0, wspace = .1)
+
+    ax_aitoff = fig.add_subplot(111, projection="aitoff")
+    ax_aitoff.grid(True)
+
+    ax_aitoff.scatter(df["theta"], df["phi"])
+
+    plt.show()
+    pass
+
+def plot_position_heatmap(df):
+    """
+
+    """
+
+    hist, xedges, yedges = np.histogram2d(df["theta"], df["phi"], bins = 100)
+    X, Y = np.meshgrid(xedges, yedges)
+
+    # fig = plt.figure(figsize=[12, 4])
+    fig = plt.figure()
+    fig.subplots_adjust(left = 0.09, bottom = 0.13, top = 0.99,
+                        right = 0.97, hspace=0, wspace = .1)
+
+    ax_aitoff = fig.add_subplot(111, projection = "aitoff")
+    ax_aitoff.grid(True)
+
+    # ax_flat = plt.subplot2grid((1,3), (0,0), colspan = 1, rowspan = 1)
+    # ax_aitoff = plt.subplot2grid((1,3), (0,1), colspan = 2, rowspan = 1, projection="aitoff")
+
+    # im_flat = ax_flat.pcolormesh(X, Y, hist.T)
+    im_aitoff = ax_aitoff.pcolormesh(X, Y, hist.T)
+
+
+    fig.colorbar(im_aitoff, ax=ax_aitoff)
+
+    plt.show()
+    pass
