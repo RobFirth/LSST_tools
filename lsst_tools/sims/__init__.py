90825358a48bab28d65eb0a7867f3ad85015315d
Rob Firth <ref1g12@soton.ac.uk>
2017-05-09 13:57:35 +0100

    added scripts dir

--- lsst_tools/sims/__init__.py
+++ lsst_tools/sims/__init__.py
@@ -30,6 +30,7 @@ import lsst_tools.utils as utils
 import lsst_tools.utils.colours as colours
 
 import pyCoCo as pccsims
+import pycoco as pcc
 
 from multiprocessing import Pool, cpu_count
 from functools import partial
@@ -114,80 +115,98 @@ def visualise_sky_coverage(df, ddf_fieldID):
         s_a.remove()
         txt.remove()
 
-def main1():
-    opsimdf= connect_to_db()
+# def main1():
+#     opsimdf= connect_to_db()
+#
+#     with closing(Pool(processes = cpu_count())) as pool:
+#         x = pool.map(partial(visualise_sky_coverage_multiproc, ), )
+#         pool.terminate()
+#     pass
+#
+#     pass
+#
+# def visualise_sky_coverage_multiproc(df, ddf_fieldID):
+#     df_to_plot = df[["fieldID", "fieldRA", "fieldDec", "cmap_visit_value"]]
+#
+#     # outpath = "/Users/berto/plots/LSST/cadence/minion/Test_skymap_night_"
+#     outpath = os.path.join(os.environ["HOME"], "plots/LSST/cadence/Fake_Rolling/skymap_night_")
+#
+#     df_to_plot["zorder"] = df_to_plot["fieldID"]*0
+#     ###
+#     # DDF fieldID???
+#     ddf = [1427,744,2412,290,2786] ## for opsimdb astro_lsst_01_1068, minion_1016
+#     # ddf = [290,2786]
+#     ###
+#     df_to_plot["zorder"].loc[df_to_plot["fieldID"].isin(ddf)] = 99
+#     norm = mpl_colors.LogNorm(vmin=max_visits, vmax=min_visits)
+#
+#     for i, group in enumerate(night_group.groups):
+#
+#         if i == 0:
+#             fig = plt.figure()
+#             fig.subplots_adjust(left = 0.05, bottom = 0.05, top = 0.99,
+#                                 right = 0.97, hspace=0, wspace = .1)
+#
+#             ax_aitoff = fig.add_subplot(111, projection = "aitoff")
+#             ax_active = fig.add_subplot(111, projection = "aitoff")
+#             ax_aitoff.grid(True)
+#             ax_active.grid(True)
+#
+#             ax_aitoff.set_xticklabels(['$2^h$','$4^h$','$6^h$', '$8^h$', '$10^h$', '$12^h$',
+#                                       '$14^h$','$16^h$','$18^h$','$20^h$','$22^h$'], fontsize=10)
+#             ax_active.set_xticklabels(['$2^h$','$4^h$','$6^h$', '$8^h$', '$10^h$', '$12^h$',
+#                                       '$14^h$','$16^h$','$18^h$','$20^h$','$22^h$'], fontsize=10)
+#
+#             ax_cbar = fig.add_subplot(20,1,19)
+#             cb1 = mpl_colorbar.ColorbarBase(ax_cbar, cmap=cmap,
+#                                     norm=norm, orientation='horizontal')
+#             cb1.set_label(r'$\textnormal{Number of Visits}$')
+#
+#             working_df_to_plot = df_to_plot.loc[night_group.groups[group]]
+#
+#         else:
+#             working_df_to_plot = pd.concat([working_df_to_plot,df_to_plot.loc[night_group.groups[group]]])
+#
+#             working_df_to_plot.drop_duplicates(subset=['fieldID'], inplace=True, keep="last")
+#
+#         s = r"$\textnormal{night} = " + str(i) + "$"
+#         txt = plt.figtext(0.85, 0.8, s)
+#
+#         s1 = ax_aitoff.scatter(working_df_to_plot["fieldRA"] - np.pi,
+#                           working_df_to_plot["fieldDec"],
+#                           color = cmap(working_df_to_plot["cmap_visit_value"]))
+#
+#
+#         s2 = ax_aitoff.scatter(working_df_to_plot.loc[df_to_plot["zorder"] == 99,["fieldRA"]] - np.pi,
+#                           working_df_to_plot.loc[df_to_plot["zorder"] == 99,["fieldDec"]],
+#                           color = cmap(working_df_to_plot[working_df_to_plot["zorder"] == 99]["cmap_visit_value"])
+#                           )
+#
+#         s_a = ax_active.scatter(df_to_plot.loc[night_group.groups[group]]["fieldRA"] - np.pi,
+#                           df_to_plot.loc[night_group.groups[group]]["fieldDec"],
+#                           color = cmap(df_to_plot.loc[night_group.groups[group]]["cmap_visit_value"]),
+#                           edgecolor = colours.hex["black"])
+#
+#
+#         plt.draw()
+#         fig.savefig(outpath + str('%05d' % i)+".png", format = 'png', dpi=200)
+#         s_a.remove()
+#         txt.remove()
+# working_df = utils.get_field_corners(working_df.drop_duplicates("fieldID"))[['fieldRA', 'fieldDec']]
 
-    with closing(Pool(processes = cpu_count())) as pool:
-        x = pool.map(partial(visualise_sky_coverage_multiproc, ), )
-        pool.terminate()
-    pass
 
-    pass
+def generate_lc(working_df, pos_df = False, n = 1):
 
-def visualise_sky_coverage_multiproc(df, ddf_fieldID):
-    df_to_plot = df[["fieldID", "fieldRA", "fieldDec", "cmap_visit_value"]]
+    if not pos_df:
+        pos_df = utils.generate_coordinates(n)
 
-    # outpath = "/Users/berto/plots/LSST/cadence/minion/Test_skymap_night_"
-    outpath = os.path.join(os.environ["HOME"], "plots/LSST/cadence/Fake_Rolling/skymap_night_")
+    fields_df = utils.get_field_corners(pd.DataFrame(working_df.drop_duplicates("fieldID"))[['fieldID', 'fieldRA', 'fieldDec']])
 
-    df_to_plot["zorder"] = df_to_plot["fieldID"]*0
-    ###
-    # DDF fieldID???
-    ddf = [1427,744,2412,290,2786] ## for opsimdb astro_lsst_01_1068, minion_1016
-    # ddf = [290,2786]
-    ###
-    df_to_plot["zorder"].loc[df_to_plot["fieldID"].isin(ddf)] = 99
-    norm = mpl_colors.LogNorm(vmin=max_visits, vmax=min_visits)
+    field_df = fields_df[(fields_df["RA_upper"] >= df["theta"][0] + np.pi) &
+                (fields_df["RA_lower"] <= df["theta"][0] + np.pi) &
+                (fields_df["Dec_lower"] >= df["phi"][0])  &
+                (fields_df["Dec_upper"] <= df["phi"][0])]
 
-    for i, group in enumerate(night_group.groups):
+    coco = pccsims.pyCoCo(b(filter_path), b(coco_root_path))
 
-        if i == 0:
-            fig = plt.figure()
-            fig.subplots_adjust(left = 0.05, bottom = 0.05, top = 0.99,
-                                right = 0.97, hspace=0, wspace = .1)
-
-            ax_aitoff = fig.add_subplot(111, projection = "aitoff")
-            ax_active = fig.add_subplot(111, projection = "aitoff")
-            ax_aitoff.grid(True)
-            ax_active.grid(True)
-
-            ax_aitoff.set_xticklabels(['$2^h$','$4^h$','$6^h$', '$8^h$', '$10^h$', '$12^h$',
-                                      '$14^h$','$16^h$','$18^h$','$20^h$','$22^h$'], fontsize=10)
-            ax_active.set_xticklabels(['$2^h$','$4^h$','$6^h$', '$8^h$', '$10^h$', '$12^h$',
-                                      '$14^h$','$16^h$','$18^h$','$20^h$','$22^h$'], fontsize=10)
-
-            ax_cbar = fig.add_subplot(20,1,19)
-            cb1 = mpl_colorbar.ColorbarBase(ax_cbar, cmap=cmap,
-                                    norm=norm, orientation='horizontal')
-            cb1.set_label(r'$\textnormal{Number of Visits}$')
-
-            working_df_to_plot = df_to_plot.loc[night_group.groups[group]]
-
-        else:
-            working_df_to_plot = pd.concat([working_df_to_plot,df_to_plot.loc[night_group.groups[group]]])
-
-            working_df_to_plot.drop_duplicates(subset=['fieldID'], inplace=True, keep="last")
-
-        s = r"$\textnormal{night} = " + str(i) + "$"
-        txt = plt.figtext(0.85, 0.8, s)
-
-        s1 = ax_aitoff.scatter(working_df_to_plot["fieldRA"] - np.pi,
-                          working_df_to_plot["fieldDec"],
-                          color = cmap(working_df_to_plot["cmap_visit_value"]))
-
-
-        s2 = ax_aitoff.scatter(working_df_to_plot.loc[df_to_plot["zorder"] == 99,["fieldRA"]] - np.pi,
-                          working_df_to_plot.loc[df_to_plot["zorder"] == 99,["fieldDec"]],
-                          color = cmap(working_df_to_plot[working_df_to_plot["zorder"] == 99]["cmap_visit_value"])
-                          )
-
-        s_a = ax_active.scatter(df_to_plot.loc[night_group.groups[group]]["fieldRA"] - np.pi,
-                          df_to_plot.loc[night_group.groups[group]]["fieldDec"],
-                          color = cmap(df_to_plot.loc[night_group.groups[group]]["cmap_visit_value"]),
-                          edgecolor = colours.hex["black"])
-
-
-        plt.draw()
-        fig.savefig(outpath + str('%05d' % i)+".png", format = 'png', dpi=200)
-        s_a.remove()
-        txt.remove()
+    return
